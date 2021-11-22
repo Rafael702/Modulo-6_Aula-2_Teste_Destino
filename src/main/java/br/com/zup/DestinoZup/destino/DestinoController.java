1de08a3e811d74caedd90d510d71a539baa80702
Vinícius <vinicius.raymundo@zup.com.br>
2021-11-22 16:18:19 -0300

    Adicionar dto de destino com validacao de dados

--- src/main/java/br/com/zup/DestinoZup/destino/DestinoController.java
+++ src/main/java/br/com/zup/DestinoZup/destino/DestinoController.java
@@ -1,18 +1,25 @@
 package br.com.zup.DestinoZup.destino;
 
+import br.com.zup.DestinoZup.destino.dto.DestinoEntradaDTO;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/destinos")
 public class DestinoController {
     @Autowired
     private DestinoService destinoService;
+    @Autowired
+    private ModelMapper modelMapper;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Destino cadastrarDestino(@RequestBody Destino destino){
-        return destinoService.salvarDestino(destino);
+    public Destino cadastrarDestino(@RequestBody @Valid DestinoEntradaDTO destino){
+        Destino destinoConvertido = modelMapper.map(destino, Destino.class);
+        return destinoService.salvarDestino(destinoConvertido);
     }
 }
