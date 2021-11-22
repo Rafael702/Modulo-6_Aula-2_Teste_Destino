2982623e2353ab5c0710c98f0db00fc47fcd2447
Vinícius <vinicius.raymundo@zup.com.br>
2021-11-22 13:39:26 -0300

    Criar repositorio para destino

---
+++ src/main/java/br/com/zup/DestinoZup/destino/DestinoRepository.java
@@ -0,0 +1,6 @@
+package br.com.zup.DestinoZup.destino;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface DestinoRepository extends CrudRepository<Destino, Integer> {
+}
