e312271fa24ead541ca2a6e729074a0ba1435f22
Vinícius <vinicius.raymundo@zup.com.br>
2021-11-22 11:19:24 -0300

    Inciar projeto

---
+++ src/main/java/br/com/zup/DestinoZup/DestinoZupApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.DestinoZup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DestinoZupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DestinoZupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/DestinoZup/DestinoZupApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.DestinoZup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DestinoZupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
