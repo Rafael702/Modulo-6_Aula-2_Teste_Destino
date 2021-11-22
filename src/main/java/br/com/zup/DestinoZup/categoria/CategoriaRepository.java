9d615eb7a675b4ca5790b336056a258afa3e512e
Vinícius <vinicius.raymundo@zup.com.br>
2021-11-22 13:46:29 -0300

    Criar repositorio para Categoria

---
+++ src/main/java/br/com/zup/DestinoZup/categoria/CategoriaRepository.java
@@ -0,0 +1,6 @@
+package br.com.zup.DestinoZup.categoria;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoriaRepository extends CrudRepository<Categoria, String> {
+}
