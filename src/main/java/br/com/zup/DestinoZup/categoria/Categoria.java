c6b5e47f1aa4e1bfc7cc891ef549a63a34326cf9
Vinícius <vinicius.raymundo@zup.com.br>
2021-11-22 16:19:08 -0300

    Adicionar validacao para nome da categoria

--- src/main/java/br/com/zup/DestinoZup/categoria/Categoria.java
+++ src/main/java/br/com/zup/DestinoZup/categoria/Categoria.java
@@ -1,11 +1,15 @@
 package br.com.zup.DestinoZup.categoria;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "categorias")
 public class Categoria {
     @Id
+    @Size(min = 4, max = 100, message = "{validacao.nome.size}")
+    @NotNull(message = "{validacao.nome.not-null}")
     private String nome;
 
     public String getNome() {
