a4ac55a6d47a13fbe0a8ceb310fb9b84846ab28e
Vinícius <vinicius.raymundo@zup.com.br>
2021-11-22 16:19:19 -0300

    Adicionar validacao para nome da regiao

--- src/main/java/br/com/zup/DestinoZup/regiao/Regiao.java
+++ src/main/java/br/com/zup/DestinoZup/regiao/Regiao.java
@@ -3,11 +3,15 @@ package br.com.zup.DestinoZup.regiao;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "regioes")
 public class Regiao {
     @Id
+    @Size(min = 4, max = 100, message = "{validacao.nome.size}")
+    @NotNull(message = "{validacao.nome.not-null}")
     private String nome;
 
     public Regiao() {
