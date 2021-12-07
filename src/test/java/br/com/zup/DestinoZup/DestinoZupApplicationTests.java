525ce942db1410ed196f137a36b3a483bb500d53
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-12-07 10:39:20 -0300

    Simular teste de salvar destino.

--- src/test/java/br/com/zup/DestinoZup/DestinoZupApplicationTests.java
+++ src/test/java/br/com/zup/DestinoZup/DestinoZupApplicationTests.java
@@ -1,13 +1,34 @@
 package br.com.zup.DestinoZup;
 
+import br.com.zup.DestinoZup.categoria.Categoria;
+import br.com.zup.DestinoZup.categoria.CategoriaRepository;
+import br.com.zup.DestinoZup.destino.DestinoRepository;
+import br.com.zup.DestinoZup.destino.DestinoService;
+import br.com.zup.DestinoZup.regiao.RegiaoRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class DestinoZupApplicationTests {
 
+	@MockBean
+	private DestinoRepository destinoRepository;
+	@MockBean
+	private RegiaoRepository regiaoRepository;
+	@MockBean
+	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private DestinoService destinoService;
+
 	@Test
-	void contextLoads() {
+	public void testarSalvarDestinoCaminhoRuim(){
+		Mockito.when(regiaoRepository.existsById(Mockito.anyString())).thenReturn(true);
 	}
+
+
 
 }
